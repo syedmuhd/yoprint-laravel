ba42974f4c11b12c74ed04b3399f235ed71845b7
Syed Muhammad Alyahya <syedmuhd.dev@gmail.com>
2025-07-15 23:04:56 +0800

    Routes for frontend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/uploads', [UploadController::class, 'index'])->name('upload.index');
+Route::post('/upload', [UploadController::class, 'store'])->name('upload.store');
