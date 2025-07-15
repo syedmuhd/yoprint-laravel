c52a84d48e08c1e3db780ac703099566417594d4
Syed Muhammad Alyahya <syedmuhd.dev@gmail.com>
2025-07-15 23:05:52 +0800

    Dockerize the entire app with dependencies to run the app. It works on my workstation, it should also work on your machine!

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Fix Laravel permissions
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+# SQLite too
+touch /var/www/html/database/database.sqlite
+chown www-data:www-data /var/www/html/database/database.sqlite
+chmod 664 /var/www/html/database/database.sqlite
+
+# Run the original PHP-FPM entrypoint
+exec docker-php-entrypoint "$@"
